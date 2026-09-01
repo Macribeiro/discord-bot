e3258f331a545eb2bd1a18f7ce0d3abf4adb9e81
Macribeiro <matheusclaudinoribeiro@gmail.com>
2026-09-01 18:51:06 -0300

    Adding new feature to play .mp3 files

--- bot.js
+++ bot.js
@@ -1,5 +1,12 @@
-require("dotenv").config();
-const { Client, GatewayIntentBits, messageLink } = require("discord.js");
+import "dotenv/config"; // Carrega seu arquivo .env automaticamente sem require
+import { Client, GatewayIntentBits, PermissionsBitField } from "discord.js";
+
+import {
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayerStatus,
+} from "@discordjs/voice";
 
 const client = new Client({
   intents: [
@@ -8,8 +15,11 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildModeration,
+    GatewayIntentBits.GuildVoiceStates,
   ],
 });
+
+const ESabado = new Date().getDay() === 2;
 
 client.once("ready", () => {
   console.log(`${client.user.tag}`);
@@ -48,6 +58,66 @@ client.on("messageCreate", async function (message) {
     message.channel.send(
       `*Caso tenha algum problema, favor reporta no [Repositório do Bot](https://github.com/Macribeiro/discord-bot)*`,
     );
+  }
+
+  const mensagemNormalizada = message.content.toLowerCase();
+
+  if (
+    mensagemNormalizada === "sabadaco" ||
+    mensagemNormalizada === "sabadaço"
+  ) {
+    console.log("Comando recebido. Verificando canal de voz...");
+
+    const voiceChannel = message.member?.voice.channel;
+
+    if (!voiceChannel) {
+      return message.channel.send(
+        "Você precisa estar em um canal de voz para ouvir o Kasino!",
+      );
+    }
+
+    try {
+      const connection = joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: message.guild.id,
+        adapterCreator: message.guild.voiceAdapterCreator,
+        selfDeafen: false,
+        selfMute: false,
+      });
+
+      const player = createAudioPlayer();
+      const resource = createAudioResource("./kasino_no_sabadaco.mp3", {
+        inlineVolume: true,
+      });
+
+      resource.volume.setVolume(0.01);
+
+      player.on(AudioPlayerStatus.Idle, () => {
+        console.log("Áudio finalizado. Desconectando do canal de voz...");
+
+        connection.destroy();
+      });
+
+      player.on("error", (error) => {
+        console.error(`[Erro no Player]: ${error.message}`);
+        connection.destroy();
+      });
+
+      if (ESabado) {
+        connection.subscribe(player);
+        player.play(resource);
+
+        message.channel.send("𝘈𝘷𝘪𝘴𝘰: 𝘢𝘣𝘢𝘪𝘹𝘦 𝘰 𝘷𝘰𝘭𝘶𝘮𝘦 𝘥𝘰 𝘣𝘰𝘵");
+
+        message.channel.send("🎵 **VAI KASINÃO!** 🎵");
+      } else {
+        message.channel.send(
+          "NUM É SÁBADO AINDA, ANIMAL. ESPERA CHEGAR O DIA PRA TOCAR",
+        );
+      }
+    } catch (error) {
+      console.error("Erro ao conectar no canal de voz:", error);
+    }
   }
 });
 
